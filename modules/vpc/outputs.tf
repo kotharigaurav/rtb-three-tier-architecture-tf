25fead8240a6b352beeff09147a422eca450e13b
Gaurav Kothari <kotharigaurav3699@gmail.com>
2026-03-23 15:59:20 +0530

    pushing code for three tier arch

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -4,14 +4,26 @@ output "vpc_id" {
 
 output "public_subnet_ids" {
   value = [
-    aws_subnet.public-1.id,
-    aws_subnet.public-2.id
+    aws_subnet.public[*].id
   ]
 }
 
+# output "public_subnet_ids" {
+#   value = [
+#     aws_subnet.public-1.id,
+#     aws_subnet.public-2.id
+#   ]
+# }
+
 output "private_subnet_ids" {
   value = [
-    aws_subnet.private-1.id,
-    aws_subnet.private-2.id
+    aws_subnet.private[*].id
   ]
 }
+
+# output "private_subnet_ids" {
+#   value = [
+#     aws_subnet.private-1.id,
+#     aws_subnet.private-2.id
+#   ]
+# }
